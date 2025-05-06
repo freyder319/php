549ef46856eeb7c46fb22b0105b76262d5a663b2
Freyder Diaz <freyderjapo@gmail.com>
2025-05-06 11:40:14 -0500

    actuqalizacion de ejercicios php 2

---
+++ actividades/pagina34.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+if ($_REQUEST['radio']=="rojo")
+setcookie("color","#ff0000",time()+60*60*24*365,"/");
+elseif ($_REQUEST['radio']=="verde")
+setcookie("color","#00ff00",time()+60*60*24*365,"/");
+elseif ($_REQUEST['radio']=="azul")
+setcookie("color","#0000ff",time()+60*60*24*365,"/");
+?>
+<html>
+<head>
+<title>Problema</title>
+</head>
+<body>
+Se creó la cookie.
+<br>
+<a href="tutorial3p34.php">Ir a la otra página</a>
+
+</body>
+</html>
+</body>
+</html>
--- actividades/tutorial3p34.php
+++ actividades/tutorial3p34.php
@@ -6,6 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    
+<html>
+<head>
+<title>Problema</title>
+</head>
+<body
+
+<?php if (isset($_COOKIE['color'])) echo " style=\"background:
+$_COOKIE[color]\"" ?>
+>
+<form action="pagina34.php" method="post">
+Seleccione de que color desea que sea la página de ahora en más:<br>
+<input type="radio" value="rojo" name="radio">Rojo<br>
+<input type="radio" value="verde" name="radio">Verde<br>
+<input type="radio" value="azul" name="radio">Azul<br>
+<input type="submit" value="Crear cookie">
+</form>
+</body>
+</html>
 </body>
 </html>
